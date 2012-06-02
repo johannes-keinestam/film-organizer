a2e192322444116b3dadc348f86d8acf745a662a
johannes.keinestam@gmail.com <johannes.keinestam@gmail.com@57026cc2-bfb9-2898-4054-9fc85a201448>
2012-06-02 00:38:12 +0000

    Added some backend code for representing and storing film items.

--- trunk/FilmOrganizer/FilmOrganizer/Film.cs
+++ trunk/FilmOrganizer/FilmOrganizer/Film.cs
@@ -4,10 +4,36 @@ using System.Linq;
 using System.Text;
 
 namespace FilmOrganizer {
-    class Film {
-        string Path;
-        int Year;
-        int Minutes;
-        string Title;
+    public class FilmCollection {
+        //private List
+    }
+    private class Film {
+        private string Path;
+        private int Year;
+        private int Minutes;
+        private string Title;
+
+        public Film(string path, int year, int minutes) {
+            this.Path = path;
+            this.Year = year;
+            this.Minutes = minutes;
+
+            GenerateTitle();
+        }
+
+        private void GenerateTitle() {
+            DummyGenerator.GenerateTitle(Path);
+        }
+    }
+
+    public interface ITitleGenerator {
+        public static string GenerateTitle(string path);
+    }
+
+    private class DummyGenerator : ITitleGenerator {
+        public static string GenerateTitle(string path) {
+            string name = path;
+            return name;
+        }
     }
 }
